6eedb36dd9a9ce9f391738d907e7899b7f242a3b
ethan <izawaryosuke0101@gmail.com>
2020-10-03 12:45:57 +0900

    hairetu

--- main.js
+++ main.js
@@ -7,20 +7,21 @@
   const btn = document.getElementById("btn");
   btn.addEventListener("click", () => {
     // console.log("hit");
-    const n = Math.floor(Math.random() * 3);
-    btn.textContent = n;
+    const results = ["大吉", "中吉", "凶", "happy"];
+    const n = Math.floor(Math.random() * results.length);
+    btn.textContent = results[n];
 
-    switch (n) {
-      case 0:
-        btn.textContent = "大吉";
-        break;
-      case 1:
-        btn.textContent = "吉";
-        break;
-      case 2:
-        btn.textContent = "凶";
-        break;
-    }
+    // switch (n) {
+    //   case 0:
+    //     btn.textContent = "大吉";
+    //     break;
+    //   case 1:
+    //     btn.textContent = "吉";
+    //     break;
+    //   case 2:
+    //     btn.textContent = "凶";
+    //     break;
+    // }
   });
 }
 
